e1373a158c8887fdcc58a6e3ba781a32a9d7bcd2
Aman Raj Rana <amanrajrana.dev@gmail.com>
2024-06-22 09:00:42 +0530

    Migrate project from Create React App to Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import {  Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import store from "./store";
 import Navbar from "./components/navbar";
 import Product from "./pages/product";
 import AddProduct from "./pages/addProduct";
-import { ToastContainer } from "react-toastify";
 import EcommProduct from "./pages/ecomm";
 import Cart from "./pages/cart";
+import { ToastContainer } from "react-toastify";
 
 // function App() {
 //   return (
@@ -20,17 +20,17 @@ import Cart from "./pages/cart";
 export default function App() {
   return (
     <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navbar />}>
-          <Route index element={<EcommProduct />} />
-          <Route path="product" element={<Product />} />
-          <Route path="addProduct" element={<AddProduct />} />
-          <Route path="cart" element={<Cart />} />
-        </Route>
-      </Routes>
-      <ToastContainer/>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<EcommProduct />} />
+            <Route path="product" element={<Product />} />
+            <Route path="addProduct" element={<AddProduct />} />
+            <Route path="cart" element={<Cart />} />
+          </Route>
+        </Routes>
+        <ToastContainer />
+      </BrowserRouter>
     </Provider>
   );
 }
