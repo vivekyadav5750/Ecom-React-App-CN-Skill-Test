79c012b9e1c610f42e68f054345b869d0ea94edc
Vivek <10Vivekkumaryadav@gmail.com>
2024-06-23 07:37:21 +0530

    finalize the editing part

--- src/components/ecom-productsCard.jsx
+++ src/components/ecom-productsCard.jsx
@@ -1,74 +1,10 @@
-import { useDispatch } from "react-redux";
+/* eslint-disable react/prop-types */
 import EcommItemCard from "./ecom-itemCard";
 
 import "react-toastify/dist/ReactToastify.css";
 
 export default function EcommProductsCard({ products, sort }) {
-  const dispatch = useDispatch();
-
-  //   const handleEditClick = (index) => {
-  //     console.log("index :: ", index);
-
-  //     const card = document.getElementById(index);
-  //     const subcard1 = card.childNodes[0];
-  //     const subcard2 = card.childNodes[1];
-
-  //     subcard1.childNodes[1].classList.remove("hidden");
-  //     subcard1.childNodes[2].classList.add("hidden");
-
-  //     subcard2.childNodes[0].classList.remove("hidden");
-  //     subcard2.childNodes[1].classList.add("hidden");
-  //   };
-
-  //   const handleCancelClick = (index) => {
-  //     const card = document.getElementById(index);
-  //     const subcard1 = card.childNodes[0];
-  //     const subcard2 = card.childNodes[1];
-
-  //     subcard1.childNodes[1].classList.add("hidden");
-  //     subcard1.childNodes[2].classList.remove("hidden");
-
-  //     subcard2.childNodes[0].classList.add("hidden");
-  //     subcard2.childNodes[1].classList.remove("hidden");
-  //   };
-
-  //   const handleDeleteClick = (index) => {
-  //     console.log("index :: ", index);
-  //     dispatch({ type: "products/deleteProduct", payload: index });
-  //     toast.error("Product Deleted Successfully!");
-  //   };
-
-  //   const handleSaveClick = (index) => {
-  //     const card = document.getElementById(index);
-  //     const subcard1 = card.childNodes[0];
-  //     const subcard2 = card.childNodes[1];
-
-  //     const title = document.getElementById(`${index}name`);
-  //     const price = document.getElementById(`${index}price`);
-  //     const rate = document.getElementById(`${index}rate`);
-  //     const description = document.getElementById(`${index}description`);
-
-  //     const product = {
-  //       title: title.value,
-  //       price: price.value,
-  //       rating: {
-  //         rate: rate.value,
-  //         count: 0,
-  //       },
-  //       description: description.value,
-  //       image: products[index].image,
-  //     };
-
-  //     dispatch({ type: "products/editProduct", payload: { index, product } });
-
-  //     subcard1.childNodes[1].classList.add("hidden");
-  //     subcard1.childNodes[2].classList.remove("hidden");
-
-  //     subcard2.childNodes[0].classList.add("hidden");
-  //     subcard2.childNodes[1].classList.remove("hidden");
-
-  //     toast.success("Product Updated Successfully!");
-  //   };
+  // const dispatch = useDispatch();
 
   const sortedProducts = sort
     ? [...products].sort((a, b) => a.price - b.price)
@@ -79,9 +15,15 @@ export default function EcommProductsCard({ products, sort }) {
     <>
       <div className="flex flex-col w-full justify-center items-center bg- ">
         {sortedProducts.map((product, index) => (
-          <EcommItemCard product={product} index={index} key={index} />
+          <EcommItemCard data={product} index={index} key={index} />
         ))}
       </div>
     </>
   );
 }
+
+// set props set
+// EcommProductsCard.propTypes = {
+//   products: PropTypes.array,
+//   sort: PropTypes.bool,
+// };
--- src/redux/productReducer.js
+++ src/redux/productReducer.js
@@ -1,89 +1,90 @@
-import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
-import { buildCreateApi } from '@reduxjs/toolkit/query';
-import CartAddProduct from '../components/addNewProduct';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    products : [],
-    loading : true,
-    Carts : [],
-}
+  products: [],
+  loading: true,
+  Carts: [],
+};
 
 export const fetchProducts = createAsyncThunk(
-    'products/fetchProducts',
-    async () => {
-        const response = await fetch('https://my-json-server.typicode.com/vivekyadav5750/dummyapi/products');
-        return response.json();
-    }
+  "products/fetchProducts",
+  async () => {
+    const response = await fetch(
+      "https://my-json-server.typicode.com/vivekyadav5750/dummyapi/products"
+    );
+    return response.json();
+  }
 );
 
 const productSlice = createSlice({
-    name : 'products',
-    initialState,
-    reducers : {
-        addProduct : (state, action) => {
-            console.log("action :: addProduct", action.payload);
-            state.products = [...state.products, {...action.payload, id : state.products.length+1}];
-            console.log("state.products :: ", state.products);
-        },
-        deleteProduct : (state, action) => {
-            console.log("action :: deleteProduct", action.payload);
-            state.products = state.products.filter((product, index) => index !== action.payload);
-            console.log("state.products :: ", state.products);
-        },
-        editProduct : (state, action) => {
-            console.log("action :: editProduct", action.payload);
-            state.products = state.products.map((product, index) => {
-                if(index === action.payload.index){
-                    return action.payload.updatedProduct;
-                }
-                return product;
-            });
-            console.log("state.products :: ", state.products);
-        },
-        addToCart : (state, action) => {
-            const isProductInCart = state.Carts.find((cart) => cart.id === action.payload.id);
-            // if there is already a product in cart then increase the quantity of that product by 1 else add the product to cart with quantity 1 
-            if(isProductInCart){
-                state.Carts = state.Carts.map((cart) => {
-                    if(cart.id === action.payload.id){
-                        return {...cart, quantity : cart.quantity + 1};
-                    }
-                    return cart;
-                });
-            }
-            else{
-                state.Carts = [...state.Carts, {...action.payload, quantity : 1}];
-            }          
-
-            console.log("state.Carts :: ", state.Carts);
-        },  
-        removeFromCart : (state, action) => {
-            // state.Carts = state.Carts.filter((cart, index) => index !== action.payload);
-            state.Carts = state.Carts.map((cart, index) => {
-                if(index === action.payload){
-                    return {...cart, quantity : cart.quantity - 1};
-                }
-                return cart;
-            }).filter((cart) => cart.quantity > 0);
-            console.log("state.Carts :: ", state.Carts);
-        }
-
+  name: "products",
+  initialState,
+  reducers: {
+    addProduct: (state, action) => {
+      console.log("action :: addProduct", action.payload);
+      state.products = [
+        ...state.products,
+        { ...action.payload, id: state.products.length + 1 },
+      ];
+      console.log("state.products :: ", state.products);
     },
-    extraReducers :(builder) => {
-        // builder.addCase(fetchProducts.pending, (state, action) => {
-        //     state.loading = true;
-        // });
-        builder.addCase(fetchProducts.fulfilled, (state, action) => {
-            state.products = action.payload;
-            state.loading = false;
-            console.log(state.products);
+    deleteProduct: (state, action) => {
+      console.log("action :: deleteProduct", action.payload);
+      state.products = state.products.filter(
+        (product) => product.id !== action.payload
+      );
+      console.log("state.products :: ", state.products);
+    },
+    editProduct: (state, action) => {
+      console.log("action :: editProduct", action.payload);
+      state.products = state.products.map((product) => {
+        if (product.id === action.payload.id) {
+          return action.payload.updatedProduct;
+        }
+        return product;
+      });
+      console.log("state.products :: ", state.products);
+    },
+    addToCart: (state, action) => {
+      const isProductInCart = state.Carts.find(
+        (cart) => cart.id === action.payload.id
+      );
+      // if there is already a product in cart then increase the quantity of that product by 1 else add the product to cart with quantity 1
+      if (isProductInCart) {
+        state.Carts = state.Carts.map((cart) => {
+          if (cart.id === action.payload.id) {
+            return { ...cart, quantity: cart.quantity + 1 };
+          }
+          return cart;
         });
-    }
+      } else {
+        state.Carts = [...state.Carts, { ...action.payload, quantity: 1 }];
+      }
 
+      console.log("state.Carts :: ", state.Carts);
+    },
+    removeFromCart: (state, action) => {
+      // state.Carts = state.Carts.filter((cart, index) => index !== action.payload);
+      state.Carts = state.Carts.map((cart, index) => {
+        if (index === action.payload) {
+          return { ...cart, quantity: cart.quantity - 1 };
+        }
+        return cart;
+      }).filter((cart) => cart.quantity > 0);
+      console.log("state.Carts :: ", state.Carts);
+    },
+  },
+  extraReducers: (builder) => {
+    // builder.addCase(fetchProducts.pending, (state, action) => {
+    //     state.loading = true;
+    // });
+    builder.addCase(fetchProducts.fulfilled, (state, action) => {
+      state.products = action.payload;
+      state.loading = false;
+      console.log(state.products);
+    });
+  },
 });
 
 export const productReducer = productSlice.reducer;
 export const productSelector = (state) => state.products;
-
-
-
